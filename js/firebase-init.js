c855172e8248c44d50cbff6989f24d974a939432
NAHID JAHAN BHUIYAN <nahidjahanb1@gmail.com>
2026-01-03 14:30:11 +0800

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -3,12 +3,13 @@
 // 1. SET UP FIREBASE CONFIGURATION
 // You must replace the text below with your OWN keys from the Firebase Console.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDkeRPtQtq4UY5zu_F4-UlkIHdMw5VW57Q",
+  authDomain: "medelife-v2.firebaseapp.com",
+  projectId: "medelife-v2",
+  storageBucket: "medelife-v2.firebasestorage.app",
+  messagingSenderId: "68690670212",
+  appId: "1:68690670212:web:be822816de83dde36b88d1",
+  measurementId: "G-N2DVL3ZWB2"
 };
 
 // 2. INITIALIZE FIREBASE
