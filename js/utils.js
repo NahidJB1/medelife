73c4b0467367fd7caf056c6aebfa3b34c54c70d2
NAHID JAHAN BHUIYAN <nahidjahanb1@gmail.com>
2026-01-20 13:21:14 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,25 @@
+// js/utils.js
+function logout() {
+    localStorage.clear();
+    window.location.href = 'index.html';
+}
 
+function showToast(msg) {
+    const b = document.getElementById('toast-box');
+    if (b) {
+        document.getElementById('toast-msg').innerText = msg;
+        b.classList.add('show');
+        setTimeout(() => b.classList.remove('show'), 3000);
+    }
+}
+
+function closeModal() {
+    const modal = document.getElementById('dashboardModal');
+    if (modal) modal.classList.remove('active');
+}
+
+// View File Helper
+function viewFile(data) { 
+    const win = window.open(); 
+    win.document.write(`<iframe src="${data}" style="width:100%;height:100%;border:none;"></iframe>`); 
+}
